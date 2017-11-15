76c3173dda5f53b0f572670ec68e17610bde47c4
inti0236 <inti0236@192.168.1.171>
2017-11-15 10:34:45 +0100

    Modeles : Ajout Role puis son association avec administrateur

---
+++ 19_ECommerce_Spring/src/main/java/fr/adaming/modele/Administrateur.java
@@ -0,0 +1,73 @@
+package fr.adaming.modele;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="administrateurs")
+public class Administrateur {
+	// Attributs
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id_a")
+	private int idAdmin;
+	private String identifiant;
+	private String mdp;
+	
+	private boolean actif;
+	
+	@OneToMany(mappedBy="administrateur")
+	@Column(name="rolename")
+	private List<Role> listeRoles;
+
+	
+	// Constructeurs
+	public Administrateur() {
+		super();
+	}
+	public Administrateur(String identifiant, String mdp) {
+		super();
+		this.identifiant = identifiant;
+		this.mdp = mdp;
+	}
+	public Administrateur(int idAdmin, String identifiant, String mdp) {
+		super();
+		this.idAdmin = idAdmin;
+		this.identifiant = identifiant;
+		this.mdp = mdp;
+	}
+	// Getters/Setters
+	public int getIdAdmin() {
+		return idAdmin;
+	}
+	public void setIdAdmin(int idAdmin) {
+		this.idAdmin = idAdmin;
+	}
+	public String getIdentifiant() {
+		return identifiant;
+	}
+	public void setIdentifiant(String identifiant) {
+		this.identifiant = identifiant;
+	}
+	public String getMdp() {
+		return mdp;
+	}
+	public void setMdp(String mdp) {
+		this.mdp = mdp;
+	}
+	//toString
+	@Override
+	public String toString() {
+		return "Administrateur [idAdmin=" + idAdmin + ", identifiant=" + identifiant + ", mdp=" + mdp + "]";
+	}
+	
+	
+}
---
+++ 19_ECommerce_Spring/src/main/java/fr/adaming/modele/Role.java
@@ -0,0 +1,52 @@
+package fr.adaming.modele;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="roles")
+public class Role {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id_role")
+	private int id;
+	private String nomRole;
+		
+	@ManyToOne
+	@JoinColumn(name="id_admin")
+	private Administrateur adminstrateur;
+	
+	public Role() {
+		super();
+	}
+	public Role(String nomRole) {
+		super();
+		this.nomRole = nomRole;
+	}
+	public Role(int id, String nomRole) {
+		super();
+		this.id = id;
+		this.nomRole = nomRole;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNomRole() {
+		return nomRole;
+	}
+	public void setNomRole(String nomRole) {
+		this.nomRole = nomRole;
+	}
+	
+	
+}
