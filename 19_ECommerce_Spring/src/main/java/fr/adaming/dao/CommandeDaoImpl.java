2b3b30f1bac4708ab959ca252fe2136747a5f199
inti-0257 <inti-0257@192.168.1.167>
2017-11-16 10:26:14 +0100

    Kevin: envoi implementation temporaire commandes

--- 19_ECommerce_Spring/src/main/java/fr/adaming/dao/CommandeDaoImpl.java
+++ 19_ECommerce_Spring/src/main/java/fr/adaming/dao/CommandeDaoImpl.java
@@ -1,5 +1,27 @@
 package fr.adaming.dao;
 
+import fr.adaming.modele.Commande;
+import fr.adaming.modele.Panier;
+import fr.adaming.modele.Produit;
+
 public class CommandeDaoImpl implements ICommandeDao {
+
+	@Override
+	public Commande enregistrementCommande(Commande commande) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Produit addProduitPanier(Produit p, int quantite, Panier pan) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int deleteProduitPanier(Produit p, Panier pan) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
